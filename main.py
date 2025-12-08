80d584745f428649a98c42792d3efcde607d0a1c
Fabrício <fabricio.matematica17@gmail.com>
2025-12-08 12:42:21 -0300

    deploy: conectando frontend da vercel ao backend do render

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ app = FastAPI(title="Plataforma EdTech Modularizada")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "https://estude.onrender.com"],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
